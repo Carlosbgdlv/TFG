62e751027d3457fcac4220d1b30e067c1d8ccb0d
obarquero <oscar.barquero@urjc.es>
2021-06-10 07:09:10 +0200

    working on rf models review

--- tfg_tools.py
+++ tfg_tools.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def data_nan_review_analysis(X,without_homa_vit_d = False):
+def data_nan_review_analysis(X,without_homa_vit_d = False,show_info_nan = False):
     """
     Function that takes as input a matrix with pats and features for one review, and gets back
     X_train_rev_imputed,pats_to_drop,feature_to_drop,imputation_data
@@ -13,7 +13,7 @@ def data_nan_review_analysis(X,without_homa_vit_d = False):
     
     
     df = df.drop('Blood_Glucose',axis = 1)
-    df = df.drop('Glycated-HB',axis = 1)
+   # df = df.drop('Glycated-HB',axis = 1)
  
     
     if without_homa_vit_d:
@@ -21,7 +21,7 @@ def data_nan_review_analysis(X,without_homa_vit_d = False):
         df = df.drop('Insulin',axis = 1) 
         df = df.drop('Vitamin-D',axis = 1)
     
-    print(df.columns)
+    #print(df.columns)
     
     #initialize vars to return
     X_train_rev_imputed = []
@@ -30,9 +30,12 @@ def data_nan_review_analysis(X,without_homa_vit_d = False):
     imputation_data = []
     #check number of NaNs per feature
     
-    for index,value in enumerate (df.isnull().sum()):
-        if value !=0:
-            print(df.columns[index],value)
+    #use a boolean to allow to show
+    if show_info_nan:
+        
+        for index,value in enumerate (df.isnull().sum()):
+            if value !=0:
+                print(df.columns[index],value)
             
     #Drop features: Blood_Glucose and Glycated_HB
     
@@ -45,7 +48,7 @@ def data_nan_review_analysis(X,without_homa_vit_d = False):
     
     return X_train_rev_imputed,pats_to_drop,feature_to_drop,imputation_data
 
-def imputing_data(X,without_homa_vit_d = False):
+def imputing_data(X,num_reviews = 3,without_homa_vit_d = False):
     """ 
     Function that check data with nans and replace them with appropriate imputation
     """
@@ -57,12 +60,10 @@ def imputing_data(X,without_homa_vit_d = False):
     imput_data = []
     
     #run over the reviews and get the X_train_imputed, pats_to_drop, feature_to_drop, and imputation values
-    for i in range(3):
+    for i in range(num_reviews):
         print("review",i)
-        print("----------")
         X_train_rev_imputed,pats_to_drop,feature_to_drop,imputation_data = data_nan_review_analysis(X_train_aux[:,i,:],without_homa_vit_d)
-        print("----------")
-        print("----------")
+
         
         X_train_imp.append(X_train_rev_imputed)
         imput_data.append(imputation_data)
